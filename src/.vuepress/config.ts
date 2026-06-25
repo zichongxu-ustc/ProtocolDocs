9d225453064e40f693d410cbf79229baca0310ab
zichongxu-ustc <zchong_xu@mail.ustc.edu.cn>
2026-06-25 15:28:30 +0800

    test

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/ProtocolDocs/",
+  base: "/",
 
   lang: "zh-CN",
   title: "博客演示",
